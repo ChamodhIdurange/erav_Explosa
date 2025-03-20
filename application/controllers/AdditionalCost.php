b2197b138308b6858edfd0cdde938a7bc5849ee7
Gayashan Malinga <84109901+Malinga0@users.noreply.github.com>
2025-03-20 19:39:28 +0530

    Updated AdditionalCost controller, model, view, and SQL tables

--- application/controllers/AdditionalCost.php
+++ application/controllers/AdditionalCost.php
@@ -48,43 +48,47 @@ class AdditionalCost extends CI_Controller
 
     public function fetch_cost_types()
     {
-        $this->load->model('AdditionalCostInfo'); 
-        $cost_types = $this->AdditionalCostInfo->get_cost_types(); 
+        $this->load->model('AdditionalCostInfo');
+        $cost_types = $this->AdditionalCostInfo->get_cost_types();
 
-        echo json_encode($cost_types, JSON_PRETTY_PRINT); 
+        echo json_encode($cost_types, JSON_PRETTY_PRINT);
     }
 
-    public function save_additional_costs() {
-		$data = $this->input->post('additionalCosts');
+    public function save_additional_costs()
+    {
+        $data = $this->input->post('additionalCosts');
         $jobQuotationId = $this->input->post('jobQuotationId');
 
-		if ($this->AdditionalCostInfo->save_additional_costs($data ,$jobQuotationId)) {
-			echo json_encode(['success' => true]);
-		} else {
-			echo json_encode(['success' => false]);
-		}
-	}
+        if ($this->AdditionalCostInfo->save_additional_costs($data, $jobQuotationId)) {
+            echo json_encode(['success' => true]);
+        } else {
+            echo json_encode(['success' => false]);
+        }
+    }
 
-    public function fetch_additional_costs_by_quotation($jobQuotationId) {
+    public function fetch_additional_costs_by_quotation($jobQuotationId)
+    {
         $data = $this->AdditionalCostInfo->get_additional_costs_by_quotation($jobQuotationId);
         echo json_encode($data);
     }
 
     public function get_additional_cost($id)
-{
+    {
 
-    $data = $this->AdditionalCostInfo->get_additional_cost_by_id($id);
-    echo json_encode($data);
-}
+        $data = $this->AdditionalCostInfo->get_additional_cost_by_id($id);
+        echo json_encode($data);
+    }
 
 
-    public function update_additional_cost() {
+    public function update_additional_cost()
+    {
         $data = $this->input->post();
         $result = $this->AdditionalCostInfo->update_additional_cost($data);
         echo json_encode(['success' => $result]);
     }
 
-    public function delete_additional_cost($id) {
+    public function delete_additional_cost($id)
+    {
         $result = $this->AdditionalCostInfo->delete_additional_cost($id);
         echo json_encode(['success' => $result]);
     }
